e91199f964760ed2fea41830e5ef0c9d0ba01da3
kanarian <arian.joyandeh@hotmail.com>
2023-09-10 22:26:41 +0200

    fix theme color

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -17,6 +17,7 @@ const Layout = ({ children, title = "This is the default title" }: Props) => (
       ></link>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="theme-color" content="#f1f5f9" />
       <meta
         name="description"
         content="Een rad waaraan je kunt draaien om een random energizer te krijgen."
